aa973a5f03df340408be5d78186c60f112b7eac4
jonte97 <jonte.9744@gmail.com>
2020-03-06 23:19:25 +0100

    asd

---
+++ ClientApp/src/functions/startupHelper.js
@@ -0,0 +1,54 @@
+export const startSummoner = {
+    profileIconId: 4353,
+        name: 'Summoner not fetched',
+        puuid: '',
+        summonerLevel: 0,
+        accountId: '',
+        id: '',
+        revisionDate: 1582835855000
+};
+
+export const startLeague = [
+    {
+        tier: 'Gold',
+        freshblood: 'asdasd',
+        inactive: 'asdasd',
+        leagueId: 'asdasd',
+        rank: 'II',
+        leaguePoints: 51,
+        losses: 12,
+        wins: 13,
+        hotStreak: true,
+        summonerName: 'a name',
+        queueType: 'RANKED_FLEX_SR',
+        veteran: true
+    },
+    {
+        tier: 'Gold',
+        freshblood: 'asdasd',
+        inactive: 'asdasd',
+        leagueId: 'asdasd',
+        rank: 'II',
+        leaguePoints: 51,
+        losses: 12,
+        wins: 13,
+        hotStreak: true,
+        summonerName: 'a name',
+        queueType: 'RANKED_FLEX_SR',
+        veteran: true
+    },
+    {
+        tier: 'Gold',
+        freshblood: 'asdasd',
+        inactive: 'asdasd',
+        leagueId: 'asdasd',
+        rank: 'II',
+        leaguePoints: 51,
+        losses: 12,
+        wins: 13,
+        hotStreak: true,
+        summonerName: 'a name',
+        queueType: 'RANKED_FLEX_SR',
+        veteran: true
+    }
+]
