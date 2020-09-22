75ef7a6b1b333ba3de779434bc7a1286b82ec1ae
jonte97 <jonte.9744@gmail.com>
2020-09-22 14:49:43 +0200

    start on runesetup

--- ClientApp/src/components/body/MatchHistoryNew/BuildComponents/RuneSetup.jsx
+++ ClientApp/src/components/body/MatchHistoryNew/BuildComponents/RuneSetup.jsx
@@ -4,18 +4,15 @@ const RuneSetup = (props) => {
 
     let primaryRunePath = props.runes.find((obj) => { return obj.id === props.stats.perkPrimaryStyle });
     let keystone = primaryRunePath.slots[0].runes.find((obj) => { return obj.id === props.stats.perk0});
+    //let perk1 = primaryRunePath.slots[0].runes.find((obj) => { return obj.id === props.stats.perk1});
+
 
     let keyStoneThumbnail = `http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`
     
 
     return (
         <div>
-            <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
-            <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
-            <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
-            <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
-            
-            <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
+            <h4>Runes</h4>
             <img className="history-item-runesetup" src={`http://ddragon.leagueoflegends.com/cdn/img/${keystone.icon}`} />
         </div>
     );
