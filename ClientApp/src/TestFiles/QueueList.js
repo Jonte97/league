a4f7a12d9579d6c949dc7de540449456264a436b
Elev <Elev@ElevdatorHT2014-2.local>
2020-10-10 14:59:07 +0200

    fixed some styling. fixed colors on psuedo winrates

--- ClientApp/src/TestFiles/QueueList.js
+++ ClientApp/src/TestFiles/QueueList.js
@@ -5,27 +5,32 @@ export const testQueueList = {
                 {
                     "matchList": null,
                     "gameCount": 58,
-                    "championId": 238
+                    "championId": 238,
+                    "winrate": 52
                 },
                 {
                     "matchList": null,
                     "gameCount": 18,
-                    "championId": 245
+                    "championId": 245,
+                    "winrate": 50
                 },
                 {
                     "matchList": null,
                     "gameCount": 13,
-                    "championId": 81
+                    "championId": 81,
+                    "winrate": 45
                 },
                 {
                     "matchList": null,
                     "gameCount": 11,
-                    "championId": 51
+                    "championId": 51,
+                    "winrate": 42
                 },
                 {
                     "matchList": null,
                     "gameCount": 9,
-                    "championId": 112
+                    "championId": 112,
+                    "winrate": 57
                 }
             ],
             "queueId": [
@@ -37,27 +42,32 @@ export const testQueueList = {
                 {
                     "matchList": null,
                     "gameCount": 121,
-                    "championId": 51
+                    "championId": 51,
+                    "winrate": 57
                 },
                 {
                     "matchList": null,
                     "gameCount": 73,
-                    "championId": 245
+                    "championId": 245,
+                    "winrate": 50
                 },
                 {
                     "matchList": null,
                     "gameCount": 56,
-                    "championId": 238
+                    "championId": 238,
+                    "winrate": 43
                 },
                 {
                     "matchList": null,
                     "gameCount": 54,
-                    "championId": 81
+                    "championId": 81,
+                    "winrate": 42
                 },
                 {
                     "matchList": null,
                     "gameCount": 51,
-                    "championId": 58
+                    "championId": 58,
+                    "winrate": 60
                 }
             ],
             "queueId": [
--- ClientApp/src/components/MainPage/RankedProfileComponents/PlayedChampionItem.jsx
+++ ClientApp/src/components/MainPage/RankedProfileComponents/PlayedChampionItem.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { getChampionImageById } from '../../../functions/ChampionHelper'
 
 const PlayedChampionItem = (props) => {
-
     const [champion, setChampion] = useState(null);
     useEffect(() => {
         const champ = getChampionImageById(props.champion.championId, props.champions);
@@ -10,14 +9,30 @@ const PlayedChampionItem = (props) => {
         console.log(champ)
     }, [props.champion]);
 
+    const winrateStyle = (winrate) => {
+        const color = {
+            color: winrate >= 50 ? "#50fb50" : "#fb5057"
+        }
+        return color;
+    }
+
     return (
         champion ?
             <div className="ranked-profile-section">
                 <div className="ranked-profile-champion-item">
                     <img className="ranked-profile-thumbnail" alt="champImg" src={`https://ddragon.leagueoflegends.com/cdn/10.19.1/img/champion/${champion.full}`} />
                     <div className="champion-item-games">
-                        <div><span> {props.champion.gameCount} Games </span></div>
-                        <div><span>52% win</span></div>
+                        <div>
+                            {/* TODO fix width */}
+                            <div className="game-count">{props.champion.gameCount}</div>
+                            <span> Games </span>
+                        </div>
+                        <div>
+                            <span style={winrateStyle(props.champion.winrate)}>
+                                {props.champion.winrate}%
+                            </span>
+                                win
+                        </div>
                     </div>
                 </div>
             </div >
